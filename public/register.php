111f655048fa08b05a4a6ef4b68b4aff18219b53
davoodya <davoodya40@gmail.com>
2024-10-20 23:44:12 +0330

    Implement Step 2: Check if the user is already logged in

--- public/register.php
+++ public/register.php
@@ -5,3 +5,14 @@
 session_start();
 
 require_once "../includes/db.php";
+
+// Step 2: Check if the user is already logged in
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: dashboard.php");
+    exit;
+}
+
+$username = $password = $confrim_password = "";
+$username_err = $password_err = $confrim_password_err = "";
+
+// Step 3: Process login form submission

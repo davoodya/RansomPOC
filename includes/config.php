228b55d2f069ab05074fa15b78a1c92b89185d4b
davoodya <davoodya40@gmail.com>
2024-10-20 20:59:57 +0330

    add gitignore

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// step 1: Configuration file for our MySQL database
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '12945');
+define('DB_NAME', 'prjrans');

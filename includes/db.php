7b08bbfdd90c5d0d2e4190872143fad06b196de3
davoodya <davoodya40@gmail.com>
2024-10-20 21:30:03 +0330

    implement config.php and db.php files to store database connection information

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+
+// Step 1: Include the configuration file that contains the database parameters
+require_once "config.php";
+
+// Step 2: Try to establish a connection to the MySql database using PDO
+try {
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+

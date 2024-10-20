5c31e8189799038f746f4e402793f4bcfa7ca6e7
davoodya <davoodya40@gmail.com>
2024-10-20 22:45:58 +0330

    Implement getGoBackLink() function to dynamically generate the 'Go Back' Link

--- includes/migrate.php
+++ includes/migrate.php
@@ -5,3 +5,14 @@ require_once "db.php";
 
 // Step 2: Include the CSS file for styling the Output
 echo "<link rel='stylesheet' href='../assets/css/migrate.css'>";
+
+// Step 3:Define a function to dynamically generate the 'Go Back' Link
+function getGoBackLink() {
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $host = $_SERVER['HTTP_HOST'];
+    $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+    return $protocol . $host . str_replace('/includes', '/public', $uri) . '/dashboard.php';
+
+}
+

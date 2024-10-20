04c95f3f775fb42da3740c4d3b396392d3042e27
davoodya <davoodya40@gmail.com>
2024-10-20 21:36:00 +0330

    implement migrate.php  to migrate db.php with stylishoutput using migrate.css

---
+++ includes/migrate.php
@@ -0,0 +1,7 @@
+<?php
+
+// Step 1: Include the Database connection file
+require_once "db.php";
+
+// Step 2: Include the CSS file for styling the Output
+echo "<link rel='stylesheet' href='../assets/css/migrate.css'>";

699744796c6cf51d23a782922dba08fb4f9a3786
davoodya <davoodya40@gmail.com>
2024-10-22 00:59:16 +0330

    Implement Step 13: Status Overview section

--- public/dashboard.php
+++ public/dashboard.php
@@ -148,6 +148,52 @@ $keys = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <h1 class="text-white">Welcome back, <?= htmlspecialchars($_SESSION["username"]); ?>!</h1>
 
 
+            <!-- Step 13: Status Overview section -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    <div class="col-sm-6 col-xl-3">
+                        <div class="bg-light-black rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-chart-line fa-3x fa-red"></i>
+                            <div class="ms-3">
+                                <p class="mb-2 text-white">Active Warriors</p>
+                                <h6 class="mb-0 text-white"><?= $activeWarriors ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-xl-3">
+                        <div class="bg-light-black rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-chart-bar fa-3x fa-red"></i>
+                            <div class="ms-3">
+                                <p class="mb-2 text-white">Total Encrypted</p>
+                                <h6 class="mb-0 text-white"><?= $totalEncrypted ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-xl-3">
+                        <div class="bg-light-black rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-chart-area fa-3x fa-red"></i>
+                            <div class="ms-3">
+                                <p class="mb-2 text-white">Machine Paid</p>
+                                <h6 class="mb-0 text-white"><?= $machinePaid ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-xl-3">
+                        <div class="bg-light-black rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-chart-pie fa-3x fa-red"></i>
+                            <div class="ms-3">
+                                <p class="mb-2 text-white">Machine Terminated</p>
+                                <h6 class="mb-0 text-white"><?= $machineTerminated ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Sale & Revenue End -->
+
+
+
+
 
 
 

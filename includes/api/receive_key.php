6c443cfcf617dc7d0f31a45a82054a82019cb5c5
davoodya <davoodya40@gmail.com>
2024-10-24 00:53:33 +0330

    Implement Step 3 & 3.1: Ensure the request method is POST and Retrieve data from the POST request

--- includes/api/receive_key.php
+++ includes/api/receive_key.php
@@ -5,4 +5,10 @@ require_once "../db.php";
 // Step 2: Set the header for JSON content type
 header('Content-Type: application/json');
 
+// Step 3: Ensure the request method is POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Step 3.1: Retrieve data from the POST request
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
 
+}
